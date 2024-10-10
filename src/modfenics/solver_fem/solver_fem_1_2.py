5ac8e484cb19652824630a01428e186bac6a210a
flecourtier <lecourtier.frederique@gmail.com>
2024-10-10 16:08:05 +0200

    TestCase1 V1 - Update

--- src/modfenics/solver_fem/solver_fem_1_2.py
+++ src/modfenics/solver_fem/solver_fem_1_2.py
@@ -1,5 +1,3 @@
-# homogeneous = True
-cd = "homo"
 print_time=False
 
 ###########
@@ -11,7 +9,6 @@ from testcases.geometry.geometry_2D import Square
 
 from dolfin import *
 import dolfin as df
-import mshr
 import time
 import numpy as np
 from pathlib import Path
@@ -23,12 +20,7 @@ parameters["allow_extrapolation"] = True
 parameters["form_compiler"]["representation"] = "uflacs"
 # parameters["form_compiler"]["quadrature_degree"] = 10
 
-# prm = parameters["krylov_solver"]
-# prm["absolute_tolerance"] = 1e-13
-# prm["relative_tolerance"] = 1e-13
-
-
-current = Path(__file__).parent.parent#.parent.parent
+current = Path(__file__).parent.parent
 
 #######
 # FEM #
@@ -40,12 +32,14 @@ class FEMSolver():
         self.params = params
         self.pb_considered = problem
         self.degree = degree
-        self.high_degree = high_degree # to compute error
         
         self.times_fem = {}
         self.times_corr_add = {}
         self.mesh,self.V,self.dx = self.__create_FEM_domain()
+        self.h = self.mesh.hmax()
         
+        # to compute error
+        self.high_degree = high_degree 
         self.V_ex = FunctionSpace(self.mesh, "CG", self.high_degree)
 
     def __create_FEM_domain(self):
@@ -68,8 +62,7 @@ class FEMSolver():
         V = FunctionSpace(mesh, "CG", self.degree)
         dx = Measure("dx", domain=mesh)
         
-        self.h = mesh.hmax()
-        print("hmax = ",self.h)
+        print("hmax = ",mesh.hmax())
 
         return mesh, V, dx
     
@@ -80,8 +73,7 @@ class FEMSolver():
         f_expr = FExpr(params, degree=self.high_degree, domain=self.mesh, pb_considered=self.pb_considered)
         u_ex = UexExpr(params, degree=self.high_degree, domain=self.mesh, pb_considered=self.pb_considered)
             
-        if cd=="homo":
-            g = Constant("0.0")
+        g = Constant("0.0")
         bc = DirichletBC(self.V, g, boundary)
 
         u = TrialFunction(self.V)
@@ -108,9 +100,6 @@ class FEMSolver():
 
         start = time.time()
         solve(A,sol.vector(),L)
-        # print("use of hypre_amg")
-        # df.solve(a==l, sol, bcs=bc, solver_parameters={"linear_solver": "cg","preconditioner":"hypre_amg"})
-        # solve(a==l, sol, bcs=bc)
         end = time.time()
 
         if print_time:
@@ -123,7 +112,8 @@ class FEMSolver():
 
         return sol,norme_L2
 
-    def corr_add(self, i, phi_tild, nonexactBC=False):
+    def corr_add(self, i, phi_tild):
+        # phi_tild defined on V_ex, error compute on V_ex
         boundary = "on_boundary"
 
         params = self.params[i]
@@ -131,13 +121,7 @@ class FEMSolver():
         u_ex = UexExpr(params, degree=self.high_degree, domain=self.mesh, pb_considered=self.pb_considered)
         f_tild = f_expr + div(grad(phi_tild))
 
-        if not nonexactBC:
-            g = Constant(0.0)
-        else:
-            g = Function(self.V)
-            # phi_tild_inter = interpolate(phi_tild, self.V)
-            phi_tild_inter = project(phi_tild, self.V)
-            g.vector()[:] = -(phi_tild_inter.vector()[:])        
+        g = Constant(0.0)
         bc = DirichletBC(self.V, g, boundary)
 
         u = TrialFunction(self.V)
