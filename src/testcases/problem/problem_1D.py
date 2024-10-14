d1d7943c5e8bcf838620d9035e8cc270e82b2f42
flecourtier <lecourtier.frederique@gmail.com>
2024-10-14 10:26:18 +0200

    TestCase2 1D - Training

--- src/testcases/problem/problem_1D.py
+++ src/testcases/problem/problem_1D.py
@@ -37,3 +37,39 @@ class TestCase1:
 
     def g(self, pre, xy, mu):
         return 0.0
+    
+class TestCase2:
+    def __init__(self):
+        self.geometry = Line1() 
+        self.nb_parameters = 2
+        self.parameter_domain = [[1.0, 2.0],[10.0, 100.0]]
+
+    def u_ex(self, pre, xy, mu):
+        if pre is dolfin:
+            x=xy[0]
+        else:
+            x=xy
+        r,Pe = mu
+        return r * (x - 1.0/(pre.exp(Pe)-1.0)*(pre.exp(Pe*x)-1.0) )
+
+    # def du_ex_dx(self, pre, xy, mu):
+    #     if pre is dolfin:
+    #         x=xy[0]
+    #     else:
+    #         x=xy
+    #     alpha,beta,gamma = mu
+    #     return 2.0*pre.pi*alpha*pre.cos(2.0*pre.pi*x) + 4.0*pre.pi*beta*pre.cos(4.0*pre.pi*x) + 6.0*pre.pi*gamma*pre.cos(6.0*pre.pi*x)
+    
+    # def d2u_ex_dx2(self, pre, xy, mu):
+    #     return -self.f(pre, xy, mu)
+
+    # def f(self, pre, xy, mu):
+    #     if pre is dolfin:
+    #         x=xy[0]
+    #     else:
+    #         x=xy
+    #     alpha,beta,gamma = mu
+    #     return pre.pi**2 * (4.0*alpha*pre.sin(2.0*pre.pi*x) + 16.0*beta*pre.sin(4.0*pre.pi*x) + 36.0*gamma*pre.sin(6.0*pre.pi*x))
+
+    def g(self, pre, xy, mu):
+        return 0.0
